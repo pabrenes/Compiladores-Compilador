109eb21bd196255f7a3e0647710bf7a6428ff0b1
Pablo Brenes <pablobrenestec@gmail.com>
2018-04-14 20:45:17 -0600

    nakho.

--- main.cpp
+++ main.cpp
@@ -10,20 +10,22 @@ int main(int argc, char* argv[]) {
 
     iniciarScanner(argv[posicionNombreArchivo]);
 
-    MuroLadrillos murito;
+    //MuroLadrillos murito;
 
-    murito.iniciarHTML("MuroLadrillos.html");
+    //murito.iniciarHTML("MuroLadrillos.html");
 
     token portaTokens = demeToken();
 
     while (portaTokens.codigoFamilia != 118){
-        murito.agregarLineaHTML(portaTokens);
+        //murito.agregarLineaHTML(portaTokens);
+        portaTokens.imprimirToken();
         portaTokens = demeToken();
+
     }
 
-    murito.cerrarHTML();
+    //murito.cerrarHTML();
 
-    murito.desplegarEstadisticas();
+    //murito.desplegarEstadisticas();
 
     finalizarScanner();
 
