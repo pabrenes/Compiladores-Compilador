573f18c9e6dc123132e5d100b2f605694d115f38
Pablo Brenes <pablobrenestec@gmail.com>
2018-04-22 18:29:39 -0600

    Código del analizador sintáctico sin manejo de errores.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
-
+#include "Gramatica.h"
 #include "parser.h"
+#include <stack>
 
 using namespace std;
 
 const int posicionNombreArchivo = 1;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     iniciarScanner(argv[posicionNombreArchivo]);
 
-    token portaTokens = demeToken();
+    token TA = demeToken();
+    stack<int> PilaParsing;
+    PilaParsing.push(NO_TERMINAL_INICIAL);
 
-    while (portaTokens.codigoFamilia != 118){
-        portaTokens.imprimirToken();
-        portaTokens = demeToken();
+    int EAP = 0;
+    int regla = 0;
 
+    while (TA.codigoFamilia != MARCA_DERECHA) {
+        EAP = PilaParsing.top();
+        PilaParsing.pop();
+        if (TERMINAL(EAP)) {
+            if (EAP == TA.codigoFamilia) {
+                TA = demeToken();
+            } else {
+                cout << "ERROR: Se esperaba un: '" << EAP << "' y obtuve un: " << TA.lexema << '\n' << '\t' << '\t'
+                     << " en linea: " << TA.fila << " columnaInicio: " << TA.columnaInicio << " columnaFin: "
+                     << TA.columnaFin;
+                return 0;
+            }
+        } else {
+            regla = TablaParsing[EAP - NO_TERMINAL_INICIAL][TA.codigoFamilia];
+            if (regla < 0) {
+                cout << "Error gramatical " << regla << '\n' << '\t' << '\t' << " en linea: " << TA.fila
+                     << " columnaInicio: " << TA.columnaInicio << " columnaFin: " << TA.columnaFin;
+                return 0;
+            } else {
+                int i = 0;
+                while ((LadosDerechos[regla][i] > -1) && (i < MAX_LADO_DER)) {
+                    PilaParsing.push(LadosDerechos[regla][i++]);
+                }
+            }
+        }
+    }
+
+    PilaParsing.pop();
+    if (!PilaParsing.empty()) {
+        cout << "Fin de archivo inesperado" << '\n';
     }
 
     finalizarScanner();
